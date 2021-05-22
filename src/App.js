7d120948c7e1221f0b40ac9d45914e43b47334f4
unknown <nesad.omeragich27@gmail.com>
2021-05-22 23:42:27 +0200

    Added react router to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Layout from './components/layout/Layout';
+import { Route } from 'react-router-dom';
 import BurgerBuilder from './containers/burgerBuilder/BurgerBuilder';
 import Checkout from './containers/checkout/Checkout';
 
@@ -6,8 +7,8 @@ function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuilder />
-        <Checkout />
+        <Route exact path='/' component={BurgerBuilder} />
+        <Route path='/checkout' component={Checkout} />
       </Layout>
     </div>
   );
